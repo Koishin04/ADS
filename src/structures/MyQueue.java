76dea7c3df94d296520eff80499dd16f2203e284
bakha <bakha@gmail.com>
2026-04-10 19:09:15 +0500

    feat: add MyStack and MyQueue logical structures

--- src/structures/MyQueue.java
+++ src/structures/MyQueue.java
@@ -1,4 +1,30 @@
 package src.structures;
 
-public class MyQueue {
+import src.implementations.MyLinkedList;
+
+public class MyQueue<T> {
+    private MyLinkedList<T> myLinkedList;
+
+    public MyQueue() {
+        myLinkedList = new MyLinkedList<>();
+    }
+
+    public void enqueue(T item) {
+        myLinkedList.addFirst(item);
+    }
+
+    public T dequeue() {
+        T item = myLinkedList.getFirst();
+        myLinkedList.removeFirst();
+        return item;
+    }
+
+    public T peek() {
+        myLinkedList.getFirst();
+        return myLinkedList.getFirst();
+    }
+
+    public boolean isEmpty() {
+        return myLinkedList.size() == 0;
+    }
 }
--- src/structures/MyStack.java
+++ src/structures/MyStack.java
@@ -1,4 +1,28 @@
 package src.structures;
 
-public class MyStack {
+import src.implementations.MyArrayList;
+
+public class MyStack<T> {
+    private MyArrayList<T> myList;
+
+    public MyStack() {
+        myList = new MyArrayList<>();
+    }
+
+    public void push(T item){
+        myList.add(item);
+    }
+
+    public T peek(){
+        if(isEmpty()) throw new java.util.EmptyStackException();
+        return myList.get(myList.size()-1);
+    }
+    public T pop(){
+        return myList.get(myList.size()-1);
+
+    }
+
+    public boolean isEmpty(){
+        return myList.size() == 0;
+    }
 }
