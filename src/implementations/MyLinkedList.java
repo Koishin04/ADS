9a408d0f09bf988c135beca7607a72b3203a7657
bakha <bakha@gmail.com>
2026-04-10 19:08:21 +0500

    feat: implement MyLinkedList using doubly linked nodes

--- src/implementations/MyLinkedList.java
+++ src/implementations/MyLinkedList.java
@@ -1,4 +1,231 @@
 package src.implementations;
 
-public class MyLinkedList {
+import src.interfaces.MyList;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class MyLinkedList<T> implements MyList<T> {
+
+    // Вложенный класс для узла [cite: 34]
+    private class MyNode {
+        T data;
+        MyNode next;
+        MyNode prev;
+
+        MyNode(T data) {
+            this.data = data;
+        }
+    }
+
+    private MyNode head; // Ссылка на первый узел [cite: 36]
+    private MyNode tail; // Ссылка на последний узел [cite: 37]
+    private int size;    // Количество элементов [cite: 38]
+
+    public MyLinkedList() {
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    }
+
+    @Override
+    public void add(T item) {
+        addLast(item);
+    }
+
+    @Override
+    public void addLast(T item) {
+        MyNode newNode = new MyNode(item);
+        if (head == null) {
+            head = tail = newNode;
+        } else {
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+        size++;
+    }
+
+    @Override
+    public void addFirst(T item) {
+        MyNode newNode = new MyNode(item);
+        if (head == null) {
+            head = tail = newNode;
+        } else {
+            newNode.next = head;
+            head.prev = newNode;
+            head = newNode;
+        }
+        size++;
+    }
+
+    @Override
+    public void add(int index, T item) {
+        if (index < 0 || index > size) throw new IndexOutOfBoundsException();
+        if (index == 0) {
+            addFirst(item);
+        } else if (index == size) {
+            addLast(item);
+        } else {
+            MyNode current = getNode(index);
+            MyNode newNode = new MyNode(item);
+            newNode.prev = current.prev;
+            newNode.next = current;
+            current.prev.next = newNode;
+            current.prev = newNode;
+            size++;
+        }
+    }
+
+    @Override
+    public T get(int index) {
+        return getNode(index).data;
+    }
+
+    private MyNode getNode(int index) {
+        if (index < 0 || index >= size) throw new IndexOutOfBoundsException();
+        MyNode current;
+        if (index < size / 2) {
+            current = head;
+            for (int i = 0; i < index; i++) current = current.next;
+        } else {
+            current = tail;
+            for (int i = size - 1; i > index; i--) current = current.prev;
+        }
+        return current;
+    }
+
+    @Override
+    public T getFirst() {
+        if (isEmpty()) throw new java.util.NoSuchElementException();
+        return head.data;
+    }
+
+    @Override
+    public T getLast() {
+        if (isEmpty()) throw new NoSuchElementException();
+        return tail.data;
+    }
+
+    @Override
+    public void set(int index, T item) {
+        getNode(index).data = item;
+    }
+
+    @Override
+    public void remove(int index) {
+        MyNode nodeToRemove = getNode(index);
+        unlink(nodeToRemove);
+    }
+
+    private void unlink(MyNode node) {
+        if (node.prev != null) node.prev.next = node.next;
+        else head = node.next;
+
+        if (node.next != null) node.next.prev = node.prev;
+        else tail = node.prev;
+
+        size--;
+    }
+
+    @Override
+    public void removeFirst() {
+        if (isEmpty()) throw new NoSuchElementException();
+        unlink(head);
+    }
+
+    @Override
+    public void removeLast() {
+        if (isEmpty()) throw new NoSuchElementException();
+        unlink(tail);
+    }
+
+    @Override
+    public int indexOf(Object object) {
+        MyNode current = head;
+        for (int i = 0; i < size; i++) {
+            if (current.data.equals(object)) return i;
+            current = current.next;
+        }
+        return -1;
+    }
+
+    @Override
+    public int lastIndexOf(Object object) {
+        MyNode current = tail;
+        for (int i = size - 1; i >= 0; i--) {
+            if (current.data.equals(object)) return i;
+            current = current.prev;
+        }
+        return -1;
+    }
+
+    @Override
+    public boolean exists(Object object) {
+        return indexOf(object) != -1;
+    }
+
+    @Override
+    public void sort() {
+        // Пузырьковая сортировка для примера (требует Comparable)
+        if (size > 1) {
+            for (int i = 0; i < size - 1; i++) {
+                MyNode current = head;
+                for (int j = 0; j < size - i - 1; j++) {
+                    if (((Comparable<T>) current.data).compareTo(current.next.data) > 0) {
+                        T temp = current.data;
+                        current.data = current.next.data;
+                        current.next.data = temp;
+                    }
+                    current = current.next;
+                }
+            }
+        }
+    }
+
+    @Override
+    public void clear() {
+        head = tail = null;
+        size = 0;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public Object[] toArray() {
+        Object[] array = new Object[size];
+        MyNode current = head;
+        int i = 0;
+        while (current != null) {
+            array[i++] = current.data;
+            current = current.next;
+        }
+        return array;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            private MyNode current = head;
+
+            @Override
+            public boolean hasNext() {
+                return current != null;
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) throw new NoSuchElementException();
+                T data = current.data;
+                current = current.next;
+                return data;
+            }
+        };
+    }
 }
