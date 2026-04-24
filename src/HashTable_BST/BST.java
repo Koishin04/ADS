2e3cfc1b31af43825f34e4f986d17748dd852e7a
bakha <bakha@gmail.com>
2026-04-25 00:51:19 +0500

    add:add package and linked

--- src/HashTable_BST/BST.java
+++ src/HashTable_BST/BST.java
@@ -1,6 +1,7 @@
+package src.HashTable_BST;
 import java.util.Iterator;
-import java.util.NoSuchElementException;
 import java.util.Stack;
+import java.util.NoSuchElementException;
 
 public class BST<K extends Comparable<K>, V> implements Iterable<BST.Node<K, V>> {
     private Node<K, V> root;
@@ -15,89 +16,52 @@ public class BST<K extends Comparable<K>, V> implements Iterable<BST.Node<K, V>>
             this.key = key;
             this.val = val;
         }
-
         public K getKey() { return key; }
         public V getValue() { return val; }
     }
 
-    public int size() {
-        return size;
-    }
+    public int size() { return size; }
 
     public void put(K key, V val) {
-        Node<K, V> newNode = new Node<>(key, val);
         if (root == null) {
-            root = newNode;
+            root = new Node<>(key, val);
             size++;
             return;
         }
-
-        Node<K, V> current = root;
-        Node<K, V> parent = null;
-
-        while (current != null) {
-            parent = current;
-            int cmp = key.compareTo(current.key);
-            if (cmp < 0) {
-                current = current.left;
-            } else if (cmp > 0) {
-                current = current.right;
-            } else {
-                current.val = val;
-                return;
-            }
+        Node<K, V> curr = root, prev = null;
+        while (curr != null) {
+            prev = curr;
+            int cmp = key.compareTo(curr.key);
+            if (cmp < 0) curr = curr.left;
+            else if (cmp > 0) curr = curr.right;
+            else { curr.val = val; return; }
         }
-
-        int cmp = key.compareTo(parent.key);
-        if (cmp < 0) parent.left = newNode;
-        else parent.right = newNode;
+        if (key.compareTo(prev.key) < 0) prev.left = new Node<>(key, val);
+        else prev.right = new Node<>(key, val);
         size++;
-    }
-
-    public V get(K key) {
-        Node<K, V> current = root;
-        while (current != null) {
-            int cmp = key.compareTo(current.key);
-            if (cmp < 0) current = current.left;
-            else if (cmp > 0) current = current.right;
-            else return current.val;
-        }
-        return null;
     }
 
     @Override
     public Iterator<Node<K, V>> iterator() {
-        return new BSTIterator(root);
-    }
+        return new Iterator<>() {
+            private Stack<Node<K, V>> stack = new Stack<>();
+            { pushLeft(root); }
 
-    private class BSTIterator implements Iterator<Node<K, V>> {
-        private Stack<Node<K, V>> stack = new Stack<>();
-
-        public BSTIterator(Node<K, V> root) {
-            pushLeftNodes(root);
-        }
-
-        private void pushLeftNodes(Node<K, V> node) {
-            while (node != null) {
-                stack.push(node);
-                node = node.left;
+            private void pushLeft(Node<K, V> node) {
+                while (node != null) {
+                    stack.push(node);
+                    node = node.left;
+                }
             }
-        }
 
-        @Override
-        public boolean hasNext() {
-            return !stack.isEmpty();
-        }
+            public boolean hasNext() { return !stack.isEmpty(); }
 
-        @Override
-        public Node<K, V> next() {
-            if (!hasNext()) throw new NoSuchElementException();
-
-            Node<K, V> node = stack.pop();
-            if (node.right != null) {
-                pushLeftNodes(node.right);
+            public Node<K, V> next() {
+                if (!hasNext()) throw new NoSuchElementException();
+                Node<K, V> node = stack.pop();
+                pushLeft(node.right);
+                return node;
             }
-            return node;
-        }
+        };
     }
 }
