ee128c28cde33b61a5061f99390484cc7fefae4d
bakha <bakha@gmail.com>
2026-04-25 00:52:30 +0500

    feat:implement tree with values

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,13 @@
+import src.HashTable_BST.BST;
 import src.HashTable_BST.MyHashTable;
 import src.HashTable_BST.MyTestingClass;
 import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        // Создаем таблицу
         MyHashTable<MyTestingClass, String> table = new MyHashTable<>(11);
         Random rnd = new Random();
 
-        // Добавляем 10 000 элементов
         for (int i = 0; i < 10000; i++) {
             MyTestingClass key = new MyTestingClass(
                     rnd.nextInt(1000000),
@@ -18,7 +17,21 @@ public class Main {
             table.put(key, "Student" + i);
         }
 
-        // Проверяем распределение
         table.printBucketSizes();
+
+
+
+
+        BST<Integer, String> tree = new BST<>();
+        tree.put(5, "Five");
+        tree.put(3, "Three");
+        tree.put(8, "Eight");
+        tree.put(1, "One");
+        tree.put(4, "Four");
+
+        System.out.println("Tree size: " + tree.size());
+        for (var elem : tree) {
+            System.out.println("key is " + elem.getKey() + " and value is " + elem.getValue());
+        }
     }
 }
